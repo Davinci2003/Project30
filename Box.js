fd708458b0a80d775a5093731c62598de0bb911e
Davinci2003 <67372840+Davinci2003@users.noreply.github.com>
2020-11-09 16:47:54 +0530

    Update Box.js

--- Box.js
+++ Box.js
@@ -4,14 +4,4 @@ class Box extends BaseClass{
       }
       display(){
         fill("purple");
-        if(this.body.speed<3){
-        super.display();
-        }
-        else{
-          World.remove(world,this.body);
-          push();
-          image(this.image,this.body.position.x,this.body.position.y,50,50);
-          pop();
-        }
-      }  
   }
